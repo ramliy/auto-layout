a0c56b3639d6f6aedd2a496e63945a5293d2ed4a
hafidzamarul <hafidzamarul@gmail.com>
2020-11-05 16:00:52 +0700

    Initial Commit

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Erick on 05/11/20.
+//  Copyright © 2020 Erick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
